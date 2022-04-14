fbce3e9adf68cfef1e61b857ba6540ad002293fe
Владислав Казанкин <v.kazankin@getqa.ru>
2022-04-14 11:02:35 +0300

    Включен боевой режим, добавил окно после успешного запуска деплоев

--- Updater/JenkinsWindow.xaml.cs
+++ Updater/JenkinsWindow.xaml.cs
@@ -469,9 +469,9 @@ namespace Updater
             {
                 String url = $"https://ci-sel.dks.lanit.ru/jenkins/job/{de.Project}/job/{de.RegisterName}/job/{de.Branch}/buildWithParameters?STAND={de.Stand}&SKIP_DB={de.SKIP_DB}&OLD_BUILD=";
                 Log.Info($"deploy \"{de.RegisterName}\" on \"{de.Stand}\" url: " + url);
-                //Requests.postRequestAsyncJenkins(url);
+                Requests.postRequestAsyncJenkins(url);
                 // Для отладки без запуска делпоя: 
-                Thread.Sleep(2000);
+                //Thread.Sleep(2000);
             }
         }
 
@@ -487,6 +487,7 @@ namespace Updater
         {
             stopLoading();
             ProjectStackPanel.IsEnabled = false;
+            MessageBox.Show("Сборки запущены");
             Log.Info("--- *** ---");
         }
 
