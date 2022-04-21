90113103ecc8bf5637f4edf4aa9beada6839ec84
Владислав Казанкин <v.kazankin@getqa.ru>
2022-04-21 16:33:12 +0300

    Деплой билдов без предварительной сборки

--- Updater/MO/PrepareNoBuildsDeployWindow.xaml.cs
+++ Updater/MO/PrepareNoBuildsDeployWindow.xaml.cs
@@ -21,11 +21,20 @@ namespace Updater.MO
     public partial class PrepareNoBuildsDeployWindow : Window
     {
         private static readonly NLog.Logger Log = NLog.LogManager.GetCurrentClassLogger();
+
         private BackgroundWorker preapareBuildsWorker = new BackgroundWorker();
+        private BackgroundWorker startDeployWorker = new BackgroundWorker();
+
         private bool loading = false;
         public PrepareNoBuildsDeployWindow()
         {
             InitializeComponent();
+
+            startDeployWorker.WorkerReportsProgress = true;
+            startDeployWorker.WorkerSupportsCancellation = true;
+            startDeployWorker.DoWork += startDeploy_DoWork;
+            startDeployWorker.ProgressChanged += startDeploy_ProgressChanged;
+            startDeployWorker.RunWorkerCompleted += startDeploy_RunWorkerCompleted;
 
             preapareBuildsWorker.WorkerSupportsCancellation = true;
             preapareBuildsWorker.WorkerReportsProgress = true;
@@ -82,6 +91,38 @@ namespace Updater.MO
             standNames.Content = standsList;
         }
 
+        private void CheckBox_Click(object sender, RoutedEventArgs e)
+        {
+            StackPanel somePanel = null;
+
+            if (sender is CheckBox checkBox)
+            {
+                if (checkBox.Name.Contains("Success"))
+                {
+                    somePanel = SuccessBuilds;
+                }
+                //if (checkBox.Name.Contains("Process"))
+                //{
+                //    somePanel = ProcessBuilds;
+                //}
+
+                if (checkBox.IsChecked == true)
+                {
+                    foreach (CheckBox box in somePanel.Children)
+                    {
+                        box.IsChecked = true;
+                    }
+                }
+                else
+                {
+                    foreach (CheckBox box in somePanel.Children)
+                    {
+                        box.IsChecked = false;
+                    }
+                }
+            }
+        }
+
         private void Cancel(object sender, RoutedEventArgs e)
         {
             Data.startedBuilds = null;
@@ -115,14 +156,57 @@ namespace Updater.MO
                     }
                 }
             }
-
+            startDeployWorker.RunWorkerAsync();
             Cancel(sender, e);
+        }
+
+        private async Task<StartingDeployResult> Deploy(Stand standBuild, Project p)
+        {
+            Log.Info("Билд: " + p.branch.name + ". Стенд: " + standBuild.Name);
+            /* request bodyJson example
+             * String json = "{'planResultKey':'EIS-EISRDIKWF40-14'," +
+                                "'name':'release-11.0.0-14'," +
+                                "'nextVersionName':'release-11.0.0-15'}";*/
+            CreateVersionBody body = new CreateVersionBody()
+            {
+                planResultKey = p.startingBuildResult.buildResultkey,
+                name = p.branch.shortName + "-" + p.startingBuildResult.buildNumber + "-" + standBuild.Name,
+                nextVersionName = p.branch.shortName + "-" + (p.startingBuildResult.buildNumber + 1) + "-" + standBuild.Name
+            };
+            String response = await Requests.postRequestAsync("https://ci-sel.dks.lanit.ru/rest/api/latest/deploy/project/" + standBuild.DeploymentProjectId + "/version", body);
+            /*
+             * response example: 
+             * {"id":82619038,
+             * "name":"hotfix-12.0.4-9-ЕИС-7",
+             * "creationDate":1643745388854,
+             * "creatorUserName":"Kazankin",
+             * "items" и так дале...
+             */
+            CreateVersionBodyResponse createVersionBodyResponse = JsonConvert.DeserializeObject<CreateVersionBodyResponse>(response);
+
+            String deployUrl = $"https://ci-sel.dks.lanit.ru/rest/api/latest/queue/deployment?environmentId={standBuild.id}&versionId={createVersionBodyResponse.id}";
+            Log.Info("deployUrl: " + deployUrl);
+            response = await Requests.postRequestAsync(deployUrl);
+            /*
+             * Response example:
+             * {"deploymentResultId":85399076,
+             * "link":{
+             *     "href":"https://ci-sel.dks.lanit.ru/rest/api/latest/deploy/result/85399076",
+             *     "rel":
+             *     "self" 
+             *     }
+             * }
+             */
+            StartingDeployResult startingDeployResult = JsonConvert.DeserializeObject<StartingDeployResult>(response);
+
+            return startingDeployResult;
         }
 
 
 
 
         private string message = "";
+
         private void PreapareBuildsWorker_DoWork(object sender, DoWorkEventArgs e)
         {
             Data.startedBuilds = new List<Project>();
@@ -177,10 +261,40 @@ namespace Updater.MO
         {
             startLoading(message);
         }
-
         private void PreapareBuildsWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
         {
             SetBuilds();
+            stopLoading();
+            Log.Info("--- *** ---");
+        }
+
+        public void startDeploy_DoWork(object sender, DoWorkEventArgs e)
+        {
+            Data.startedDeploys = new List<StartedDeploy>();
+            startDeployWorker.ReportProgress(1);
+            Log.Info("--- Начало деплоев ---");
+            foreach (PreparedDeploy deploy in Data.preparedDeploy)
+            {
+                StartingDeployResult depRes = Deploy(deploy.StandEnvironment, deploy.Project).Result;
+                StartedDeploy startedDeploy = new StartedDeploy()
+                {
+                    DeployResult = depRes,
+                    Project = deploy.Project,
+                };
+                Data.startedDeploys.Add(startedDeploy);
+            }
+        }
+
+        public void startDeploy_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            if (!loading)
+            {
+                startLoading();
+            }
+        }
+
+        public void startDeploy_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
             stopLoading();
             Log.Info("--- *** ---");
         }
@@ -190,7 +304,6 @@ namespace Updater.MO
             LoadingGrid.Visibility = Visibility.Visible;
             loading = true;
         }
-
         public void startLoading(string message)
         {
             Message.Content = message;
@@ -199,43 +312,10 @@ namespace Updater.MO
                 loading = true;
             }
         }
-
         public void stopLoading()
         {
             LoadingGrid.Visibility = Visibility.Hidden;
             loading = false;
-        }
-
-        private void CheckBox_Click(object sender, RoutedEventArgs e)
-        {
-            StackPanel somePanel = null;
-
-            if (sender is CheckBox checkBox)
-            {
-                if (checkBox.Name.Contains("Success"))
-                {
-                    somePanel = SuccessBuilds;
-                }
-                //if (checkBox.Name.Contains("Process"))
-                //{
-                //    somePanel = ProcessBuilds;
-                //}
-
-                if (checkBox.IsChecked == true)
-                {
-                    foreach (CheckBox box in somePanel.Children)
-                    {
-                        box.IsChecked = true;
-                    }
-                }
-                else
-                {
-                    foreach (CheckBox box in somePanel.Children)
-                    {
-                        box.IsChecked = false;
-                    }
-                }
-            }
         }
     }
 }
